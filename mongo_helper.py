dc4779e141cdce04dd48ce4c9ad46b5269d89ad1
pavankirigeri <48946603+pavankirigeri@users.noreply.github.com>
2019-10-11 12:13:29 +0530

    Update mongo_helper.py

--- mongo_helper.py
+++ mongo_helper.py
@@ -1,11 +1,13 @@
 from pymongo import MongoClient
 
 class MongoHelper():
+    #connect to mongodb
     def __init__(self, *args, **kwargs):
         self.client = MongoClient()
         self.db = self.client['yolo_data']
         self.mycol = self.db['video_info_times']
-    
+        
+    #get all data
     def all_data(self):
         data = []
         cursor = self.mycol.find({}).sort('timestamp', -1)
